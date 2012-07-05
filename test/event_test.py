1b583159add9106f9bfe0dad9023b74174171e2c
sweemeng <sweester@gmail.com>
2012-07-05 21:41:29 +0800

    adding a json checker

--- test/event_test.py
+++ test/event_test.py
@@ -1,11 +1,21 @@
 from event.event import Event
+import json
 
 def test_create_with_param():
     event = Event('create',{'a':1},{'b':2})
 
     test_dict = event.to_dict()
     
+    dict_checker(test_dict)
+
+    test_json = event.to_json()
+
+    test_dict = json.loads(test_json)
+    dict_checker(test_dict)
+    
+def dict_checker(test_dict):
     assert test_dict['action'] == 'create'
     assert test_dict['data'] == {'a':1}
     assert test_dict['param'] == {'b':2}
+
 
