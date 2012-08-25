71d076be0d73432ea19e43df66d152ecf12c5298
sweemeng <sweester@gmail.com>
2012-08-25 22:17:30 +0800

    make it able to use a list of entries in api, and get db function,

--- projectmodel/model.py
+++ projectmodel/model.py
@@ -13,10 +13,14 @@ class Project(object):
         self.project = ProjectTemplate()
     
     def get_api(self):
-        return '/api/%s/' % self.project.name.replace(' ','_') 
+        api_list = []
+        name = self.to_mongo_name()
+        for entry in self.project.entries:
+            api_list.append('/api/db/%s/%s/' % (name,entry))
+        return api_list 
     
     def get_url(self):
-        return '/%s/' % self.project.name.replace(' ','_')
+        return '/project/%s/' % (self.project.name.replace(' ','_'))
      
     def create(self,name,description):
         self.project.name = name
@@ -43,6 +47,19 @@ class Project(object):
     def to_mongo_name(self):
         return self.project.name.replace(' ','_')
 
+    def add_entries(self,name):
+        if name not in self.project.entries:
+            self.project.entries.append(name)
+            self.save()
+    
+    def get_db(self):
+        mongo_model = model.MongoModel(project=self.to_mongo_name())
+        temp_entries = mongo_model.db.collection_names()
+        for entry in temp_entries:
+            if entry not in self.project.entries:
+                self.create_entries(entry)
+        return [model.MongoModel(self.project.name_to_mongo(),entry) for entry in self.project.entries]
+
 
 class ProjectList(object):
     def __init__(self):
@@ -60,6 +77,17 @@ class ProjectList(object):
             id = str(entry['_id'])
             project.get(id)
             yield project
+    
+    def list(self):
+        result = self.model.all()
+        temp = []
+        for entry in result:
+            project = Project()
+            id = str(entry['_id'])
+            project.get(id)
+            temp.append(project)
+        print temp 
+        return temp
 
 
 class ProjectTemplate(object):
@@ -67,11 +95,13 @@ class ProjectTemplate(object):
         self.id = ''
         self.name = ''
         self.description = ''
+        self.entries = []
        
     def to_mongo(self):
         data = {}
         data['name'] = self.name
         data['description'] = self.description
+        data['entries'] = self.entries
         return data
     
     def from_mongo(self,data):
@@ -82,6 +112,12 @@ class ProjectTemplate(object):
                 setattr(self,'id',str(data['_id']))
                 continue
             setattr(self,key,data[key])
+
+        mongo_model = model.MongoModel(project=self.name_to_mongo())
+        coll = mongo_model.db.collection_names()
+        for c in coll:
+            if c not in self.entries:
+                self.entries.append(c)
     
     def name_to_mongo(self):
         return self.name.replace(' ','_')
